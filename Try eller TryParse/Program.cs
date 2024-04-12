c876985dcdcb8cb0a10a2150397d6134cbcc0db0
Leo Sundberg <leo.sundberg@elev.ga.ntig.se>
2024-04-12 17:03:34 +0200

    small tryparse thing to show that  stuff

---
+++ Try eller TryParse/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("WRITE ME A NUMBER NILLY WILLY");
+string input = Console.ReadLine();
+while (!int.TryParse(input, out int value))
+{
+    Console.WriteLine("NO A NUMBER YOU SILLY DRAGOON");
+    input = Console.ReadLine();
+}
+Console.WriteLine($"WHOA {input} THAT IS INDEED A NUMBER");
+Console.ReadLine();
